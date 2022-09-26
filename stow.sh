1e9de38854d561182d8c03b5aecf0cf8c466192c
Erdem Bozkurt <erdem@alvalabs.io>
2022-09-26 23:23:47 +0200

    Update stow.sh to unstow easily when needed

--- stow.sh
+++ stow.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
- directories="zsh vim tmux fzf git asdf"
- stow $directories
- # add -D flag to unstow
-#  stow -D $directories
+directories="zsh vim tmux fzf git asdf"
+if [ "$1" = "-D" ]; then
+  # add -D flag to unstow
+  stow -D $directories
+else
+  stow $directories
+fi
