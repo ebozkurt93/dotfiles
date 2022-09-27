d3b624958cb54377d19e919494f73d8e95391ac4
Erdem Bozkurt <erdem@alvalabs.io>
2022-09-27 19:30:23 +0200

    adjust iterm2 settings

---
+++ README.md
@@ -0,0 +1,3 @@
+### Configure iTerm2 settings
+Select the following path in iTerm2 options "General -> Preferences -> Load preferences from a custom folder or URL"
+`~/dotfiles/iterm2`
--- stow.sh
+++ stow.sh
@@ -3,7 +3,7 @@
 # .DS_Store files are creating issues when running stow
 find . -name ".DS_Store" -delete
 
-directories="zsh vim tmux fzf git asdf bitbar iterm2"
+directories="zsh vim tmux fzf git asdf bitbar"
 if [ "$1" = "-D" ]; then
   # add -D flag to unstow
   stow -D $directories
