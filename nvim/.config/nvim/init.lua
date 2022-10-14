74114df66f224cb73a48a66de62bd136f07fa34a
Erdem Bozkurt <erdem@alvalabs.io>
2022-10-14 20:23:03 +0200

    add more nvim keybindings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,6 @@
 -- todo: move all this content into subfolders and source them depending on the need
 
 
-
-
-
 vim.opt.encoding = 'utf-8'
 vim.opt.number = true
 vim.opt.relativenumber = false
@@ -31,6 +28,28 @@ end, {desc = 'Clear search highlights'})
 vim.keymap.set('n', '<leader>r', '<cmd>set invrelativenumber<cr>', {desc = 'Toggle relative line numbers'})
 vim.keymap.set('n', '<leader>sv', '<cmd>source $MYVIMRC<cr>', {desc = 'Source neovim config'})
 
+-- center things after jump
+vim.keymap.set('n', '<C-u>', '<C-u>zz', {noremap = true})
+vim.keymap.set('n', '<C-d>', '<C-d>zz', {noremap = true})
 
-vim.keymap.set('i', 'jk', '<ESC>', {})
+vim.keymap.set('i', 'jk', '<ESC>', {noremap = true})
+
+-- yank to clipboard
+vim.keymap.set('v', '<leader>y', '"+y', {noremap = true})
+vim.keymap.set('v', '<leader>y', '"+y', {noremap = true})
+
+-- move lines up/down
+vim.keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', {noremap = true})
+vim.keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', {noremap = true})
+vim.cmd([[
+inoremap <A-j> <Esc>:m .+1<CR>==gi
+inoremap <A-k> <Esc>:m .-2<CR>==gi
+vnoremap <A-j> :m '>+1<CR>gv=gv
+vnoremap <A-k> :m '<-2<CR>gv=gv
+]])
+-- this doesn't work for some reason, tries to do something on marks
+-- vim.keymap.set('v', '<A-j>', "<cmd>m '>+1<cr>gv=gv", {noremap = true})
+-- vim.keymap.set('v', '<A-k>', "<cmd>m '<-2<cr>gv=gv", {noremap = true})
+
+
 
