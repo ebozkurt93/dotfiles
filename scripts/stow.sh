0851454ce73fa363d6586d4847d8647e0fdf76bb
Erdem Bozkurt <ebozkurt93@gmail.com>
2022-12-10 17:25:30 +0100

    echo impacted directories

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -8,7 +8,9 @@ find . -name ".DS_Store" -delete
 directories="zsh vim nvim kitty tmux fzf git asdf bitbar starship bemlo helper_scripts"
 if [ "$1" = "-D" ]; then
   # add -D flag to unstow
+  echo "Unstowing directories: ${directories[@]}"
   stow -D $directories
 else
+  echo "Stowing directories: ${directories[@]}"
   stow $directories
 fi
