69f4131a32e870c6bf851dddaf6ccb30edc0af3d
Erdem Bozkurt <ebozkurt93@gmail.com>
2022-12-28 22:52:08 +0100

    remove remaining packer related code

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,6 @@
 -- this needs to be on top, since everything is resetted with reload
 require("ebozkurt.reload")
 require("ebozkurt.prefs")
--- require("ebozkurt.plugins")
 require("ebozkurt.lazy")
 require("ebozkurt.keymaps")
 require("ebozkurt.telescope")
