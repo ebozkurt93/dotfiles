9aa6173911bab37dda063c92610dd65f4f77f3e4
Erdem Bozkurt <ebozkurt93@gmail.com>
2023-01-27 17:57:09 +0100

    update default dock items && add default for set_wallpaper

--- helper_scripts/bin/helpers/set_wallpaper.sh
+++ helper_scripts/bin/helpers/set_wallpaper.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
+function set_wallpaper() {
+	osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${1}\" as POSIX file"
+}
+
+if [[ "$1"  == "default" ]]; then
+	set_wallpaper "/System/Library/Desktop Pictures/Chroma Red.heic"
+fi
+
 file_path=~/Documents/bitbar_plugins/tmp/wallpaper.txt
 if [[ ! -f $file_path ]]; then
 	echo invalid file path
 	exit
 fi
 content="$(head -n 1 $file_path)"
-if [[ ! "$content" =~ .(png|jpg) ]]; then
+if [[ ! "$content" =~ .(png|jpg|heic) ]]; then
 	echo invalid content in $file_path
 	echo invalid content in $content
 	exit
 fi
-osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${content}\" as POSIX file"
+
+set_wallpaper $content
 
