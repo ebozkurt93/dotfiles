f1460ed78698ab4f30c60e2bb1283e8cc709871e
Erdem Bozkurt <ebozkurt93@gmail.com>
2023-04-07 21:42:37 +0200

    nerd font fixes for kitty & updates for nvim reset

--- scripts/reset-nvim.sh
+++ scripts/reset-nvim.sh
@@ -3,6 +3,8 @@
 rm -rf ~/.local/share/nvim/site
 # depending on the need, could delete entire nvim directory
 # rm -rf ~/.local/share/nvim/
+# this is needed for the possession plugin
+# mkdir -p ~/.local/share/nvim/sessions
 
 # this could be needed so that treesitter parser takes precedence
 #  https://github.com/nvim-treesitter/nvim-treesitter/issues/3970#issuecomment-1353836834
