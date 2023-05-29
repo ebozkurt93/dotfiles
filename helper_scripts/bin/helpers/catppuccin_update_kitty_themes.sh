ed88ae83a472ff2eb11ae8e09953a8ef1d9bc03a
Erdem Bozkurt <ebozkurt93@gmail.com>
2023-05-29 17:05:08 +0200

    update kitty themes

--- helper_scripts/bin/helpers/catppuccin_update_kitty_themes.sh
+++ helper_scripts/bin/helpers/catppuccin_update_kitty_themes.sh
@@ -3,5 +3,5 @@ kitty_theme_locations="$HOME/dotfiles/kitty/.config/kitty/themes"
 variants=('latte' 'frappe' 'mocha' 'macchiato')
 for p in ${variants[@]}; do
 	echo $p
-	curl https://raw.githubusercontent.com/catppuccin/kitty/main/$p.conf > "$kitty_theme_locations/catppuccin-$p.conf"
+	curl https://raw.githubusercontent.com/catppuccin/kitty/main/themes/$p.conf > "$kitty_theme_locations/catppuccin-$p.conf"
 done
