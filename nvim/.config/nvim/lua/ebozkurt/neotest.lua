59a7020246d69f7b7aa078a84594c0d8e5312f9f
Erdem Bozkurt <ebozkurt93@gmail.com>
2023-07-21 08:23:10 +0200

    add jest to neotest

--- nvim/.config/nvim/lua/ebozkurt/neotest.lua
+++ nvim/.config/nvim/lua/ebozkurt/neotest.lua
@@ -1,16 +1,20 @@
-local neotest = require('neotest')
+local neotest = require("neotest")
 neotest.setup({
-  adapters = {
-    require("neotest-python")({
-      dap = { justMyCode = false, gevent = true },
-	  runner = "pytest",
-	  python = require('ebozkurt.helpers').get_python_path(vim.fn.getcwd()),
-	  -- args = {'--no-header', '--no-summary', '-q'},
-	  args = {'-s'},
-    }),
-    require("neotest-go")({}),
-	require("neotest-jest")({}),
+   requires = {
+    'haydenmeade/neotest-jest',
   },
+	adapters = {
+		require("neotest-python")({
+			dap = { justMyCode = false, gevent = true },
+			runner = "pytest",
+			python = require("ebozkurt.helpers").get_python_path(vim.fn.getcwd()),
+			-- args = {'--no-header', '--no-summary', '-q'},
+			args = { "-s" },
+		}),
+		require("neotest-go")({}),
+		require("neotest-jest")({
+		}),
+	},
 })
 
-vim.api.nvim_exec_autocmds('User', {pattern = 'neotest'})
+vim.api.nvim_exec_autocmds("User", { pattern = "neotest" })
