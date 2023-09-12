685e350783f7ae166056063bd6dd61f9a979440e
Erdem Bozkurt <ebozkurt93@gmail.com>
2023-09-12 23:32:53 +0200

    add power source to tmux status right

--- helper_scripts/bin/helpers/tmux_battery.sh
+++ helper_scripts/bin/helpers/tmux_battery.sh
@@ -1,5 +1,12 @@
 charging=$(pmset -g batt | tail -n 1 | awk -F ' ' 'gsub(";", "", $0) {print "BAT:", $3, $4}')
 charging=$(echo $charging | sed -e 's/discharging//g' -e 's/charging//g' -e 's/charged/-/g' -e 's/finishing/-/g')
+pwr_source=$(pmset -g batt | head -n 1 | cut -c 17- | sed "s/'//g" | xargs)
+
+if [[ "$pwr_source" == 'AC Power' ]]; then
+	pwr_source='󱐥'
+else
+	pwr_source=''
+fi
 
 pwr=$(pmset -g | grep lowpowermode | awk -F' ' '{ print$2 }')
 if [[ $pwr == '1' ]]; then
@@ -7,5 +14,5 @@ if [[ $pwr == '1' ]]; then
 else
 	pwr=''
 fi
-echo $charging $pwr
+echo $charging $pwr $pwr_source
 
