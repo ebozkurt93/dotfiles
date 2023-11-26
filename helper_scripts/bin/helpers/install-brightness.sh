b62ac9682b85fa2f3b8370d27adc96baed4be73c
Erdem Bozkurt <ebozkurt93@gmail.com>
2023-11-26 18:56:25 +0100

    toggle brightness with brightness cli

---
+++ helper_scripts/bin/helpers/install-brightness.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/nriley/brightness.git
+cd brightness
+make
+sudo make install
+cd ..
+rm -rf brightness
---
+++ helper_scripts/bin/helpers/toggle-brightness.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+b=/usr/local/bin/brightness
+
+val=$($b -lv 2>/dev/null  | awk '/display/ && /brightness/ {print $NF}' | tail -n 1)
+
+if [ "$(echo "$val < 0.10" | bc)" -eq 1 ]; then
+  $b 0.50
+else
+  $b 0
+fi
