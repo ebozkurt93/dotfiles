e1dc4d4d712e7c5b89827f5e527d8e0d5bb2c664
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-01-14 22:32:43 +0100

    add google meet mic toggle

---
+++ hammerspoon/.hammerspoon/google_meet_mic_toggle.lua
@@ -0,0 +1,35 @@
+local function browser(browserName)
+    local function open()
+        hs.application.launchOrFocus(browserName)
+    end
+    local function jump(url)
+        local script = ([[(function() {
+      var browser = Application('%s');
+      browser.activate();
+
+      for (win of browser.windows()) {
+        var tabIndex =
+          win.tabs().findIndex(tab => tab.url().match(/%s/));
+
+        if (tabIndex != -1) {
+          win.activeTabIndex = (tabIndex + 1);
+          win.index = 1;
+        }
+      }
+    })();
+  ]]):format(browserName, url)
+   hs.osascript.javascript(script)
+    end
+    return { open = open, jump = jump }
+end
+
+hs.hotkey.bind({"alt"}, "l", function()
+    local currentWindow = hs.window.focusedWindow()
+    local chrome = browser("Google Chrome")
+    chrome.jump("meet.google.com")
+    local newWindow = hs.window.focusedWindow()
+    if string.match(newWindow:title(), "Meet - ") then
+        hs.eventtap.keyStroke({"cmd"}, "D")
+    end
+    currentWindow:focus()
+end)
--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -5,5 +5,7 @@ hs.hotkey.bind(globals.hyper, "d", hs.toggleConsole)
 require("reload")
 require("window_manager")
 require("fuzzy_window_switcher")
+-- temporarily disabled
+-- require("google_meet_mic_toggle")
 
 hs.alert.show("Config loaded")
