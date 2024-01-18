0d33a1cb8f1efec9c15816a1d100d198617a3512
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-01-18 22:57:47 +0100

    migrate some operations from keyboard maestro

--- hammerspoon/.hammerspoon/helpers.lua
+++ hammerspoon/.hammerspoon/helpers.lua
@@ -1,9 +1,13 @@
 P = function(v)
-	print(hs.inspect.inspect(v))
-	return v
+  print(hs.inspect.inspect(v))
+  return v
 end
 
 M = {}
+
+function M.escape_magic(s)
+  return (s:gsub("[%^%$%(%)%%%.%[%]%*%+%-%?]", "%%%1"))
+end
 
 function M.isModuleAvailable(name)
   if package.loaded[name] then
@@ -40,6 +44,75 @@ function M.merge(...)
     end
   end
   return result
+end
+
+function M.hotkeyScopedToApp(mods, key, appName, func)
+  local yourHotkey = hs.hotkey.new(mods, key, function()
+    local app = hs.application.frontmostApplication()
+    if app then
+      func(app)
+    end
+  end)
+
+  local appFilter = hs.window.filter.new(appName)
+
+  appFilter
+    :subscribe(hs.window.filter.windowFocused, function()
+      yourHotkey:enable()
+    end)
+    :subscribe(hs.window.filter.windowUnfocused, function()
+      yourHotkey:disable()
+    end)
+end
+
+function M.keystrokesScopedToApp(target, app, func)
+  local keystrokeBuffer = ""
+
+  local function handleKeystrokes(event)
+    local currentApp = hs.application.frontmostApplication()
+
+    if currentApp:title() == app then
+      local character = event:getCharacters()
+      keystrokeBuffer = keystrokeBuffer .. character
+
+      if #keystrokeBuffer > #target then
+        keystrokeBuffer = keystrokeBuffer:sub(-#target)
+      end
+
+      if keystrokeBuffer == target then
+        keystrokeBuffer = ""
+        func()
+      end
+    end
+  end
+
+  local keystrokeTap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, handleKeystrokes)
+  keystrokeTap:start()
+  return keystrokeTap, keystrokeBuffer
+end
+
+function M.hotkeyExcludingApp(mods, key, appName, func)
+  local yourHotkey = hs.hotkey.new(mods, key, func)
+
+  local appFilter = hs.window.filter.new()
+  appFilter = appFilter:rejectApp(appName)
+  P(appFilter)
+
+  appFilter
+    :subscribe(hs.window.filter.windowFocused, function()
+      yourHotkey:enable()
+    end)
+    :subscribe(hs.window.filter.windowUnfocused, function()
+      yourHotkey:disable()
+    end)
+end
+
+function M.isCurrentWindowInFullScreen()
+  local focusedWindow = hs.window.focusedWindow()
+  if focusedWindow then
+    return focusedWindow:isFullScreen()
+  end
+  return false
 end
 
 return M
--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,13 +1,148 @@
 local globals = require("globals")
+local helpers = require("helpers")
 
 -- hs.loadSpoon("EmmyLua") -- enable when want to regenerate lua annotations for lsp
 hs.hotkey.bind(globals.hyper, "d", hs.toggleConsole)
+
+helpers.hotkeyScopedToApp({ "cmd" }, "r", "Spotify", function()
+  local app = hs.application.frontmostApplication()
+  if app then
+    app:kill()
+    hs.timer.doAfter(1, function()
+      hs.application.launchOrFocus("Spotify")
+    end)
+  end
+end)
+
+helpers.hotkeyScopedToApp({ "cmd" }, "r", "Obsidian", function(app)
+  app:selectMenuItem({ "View", "Force Reload" })
+end)
+
+helpers.hotkeyScopedToApp({ "cmd" }, "m", "Obsidian", function(app)
+  app:selectMenuItem({ "Window", "Minimize" })
+end)
+
+helpers.hotkeyScopedToApp({ "cmd" }, "f", "Pocket Casts", function(app)
+  if helpers.isCurrentWindowInFullScreen() then
+    app:selectMenuItem({ "Window", "Exit Full Screen" })
+  else
+    app:selectMenuItem({ "Window", "Enter Full Screen" })
+  end
+end)
+
+helpers.hotkeyScopedToApp({ "cmd", "alt" }, "f", "VLC", function(app)
+  app:selectMenuItem({ "Video", "Float on Top" })
+end)
+
+helpers.hotkeyScopedToApp({ "cmd" }, "a", "MultiViewer for F1", function(app)
+  app:selectMenuItem({ "Window", "Bring All to Front" })
+end)
+
+-- Google Chrome
+helpers.hotkeyScopedToApp({ "cmd", "shift" }, "0", "Google Chrome", function(app)
+  app:selectMenuItem({ "Profiles", "Erdem" })
+end)
+
+-- todo: move this to work part
+helpers.hotkeyScopedToApp({ "cmd", "shift" }, "9", "Google Chrome", function(app)
+  app:selectMenuItem({ "Profiles", "Bemlo" })
+end)
+
+helpers.hotkeyScopedToApp({ "cmd", "shift" }, "8", "Google Chrome", function(app)
+  app:selectMenuItem({ "Profiles", "VPN" })
+end)
+
+helpers.hotkeyScopedToApp({ "ctrl" }, "d", "Google Chrome", function(app)
+  app:selectMenuItem({ "Tab", "Duplicate tab" })
+end)
+
+helpers.hotkeyScopedToApp({ "alt" }, "d", "Google Chrome", function()
+  hs.eventtap.keyStroke({ "cmd" }, "l", 0)
+  hs.eventtap.keyStroke({ "cmd" }, "c", 0)
+  hs.eventtap.keyStroke({}, "escape", 0)
+  hs.eventtap.keyStroke({ "cmd", "shift" }, "n", 0)
+  hs.eventtap.keyStroke({ "cmd" }, "l", 0)
+  hs.eventtap.keyStroke({ "cmd" }, "v", 0)
+  hs.eventtap.keyStroke({}, "return", 0)
+end)
+
+helpers.hotkeyScopedToApp({ "cmd" }, "g", "Google Chrome", function()
+  hs.eventtap.rightClick(hs.mouse.absolutePosition())
+  hs.eventtap.keyStroke({}, "g", 0)
+  hs.eventtap.keyStroke({}, "return", 0)
+end)
+
+helpers.hotkeyScopedToApp({ "ctrl" }, "m", "Google Chrome", function(app)
+  app:selectMenuItem({ "Tab", "Mute site" })
+  local title = hs.window.focusedWindow():title()
+  hs.notify
+    .new({
+      title = "Chrome Mute Toggle",
+      informativeText = "Muting -" .. title,
+      autoWithdraw = true, -- Automatically dismiss the notification after a few seconds
+      withdrawAfter = 5, -- Duration in seconds before auto-dismissing
+    })
+    :send()
+end)
+
+helpers.hotkeyScopedToApp({ "cmd", "alt" }, "t", "Google Chrome", function()
+  hs.eventtap.rightClick(hs.mouse.absolutePosition())
+  hs.eventtap.keyStroke({}, "t", 0)
+  hs.eventtap.keyStroke({}, "return", 0)
+end)
+
+helpers.hotkeyScopedToApp({ "cmd", "ctrl" }, "t", "Google Chrome", function(app)
+  app:selectMenuItem({ "Tab", "New Tab to the Right" })
+end)
+
+helpers.hotkeyScopedToApp({ "cmd", "ctrl" }, "p", "Google Chrome", function(app)
+  app:selectMenuItem({ "Tab", "Pin tab" })
+end)
+
+helpers.hotkeyScopedToApp({ "ctrl", "alt" }, "m", "Google Chrome", function(app)
+  app:selectMenuItem({ "Tab" })
+end)
+
+helpers.hotkeyExcludingApp({ "alt", "shift" }, "d", "Google Chrome", function()
+  hs.execute("open ~/Downloads")
+end)
+
+local _ = helpers.keystrokesScopedToApp("bb ", "Google Chrome", function()
+  -- todo: refactor this and extract to a function
+  local _, url =
+    hs.osascript.applescript('tell application "Google Chrome" to return URL of active tab of front window')
+
+  local start = "https://www.reddit.com/"
+  if url:sub(1, #start) ~= start then
+    return
+  end
+
+  local script = [[
+var chrome = Application("Google Chrome");
+
+var command = `
+    (function() {
+
+// toggle reddit theme
+document.getElementById('USER_DROPDOWN_ID').click();
+[...document.querySelectorAll("span")].filter(s => s.textContent === 'Dark Mode')[0].click()
+//document.getElementsByClassName('icon icon-night')[0].click();
+    })();
+
+`
+chrome.windows[0].activeTab.execute({javascript:command})
+  ]]
+  hs.osascript.javascript(script:format("Google Chrome"))
+
+  hs.eventtap.keyStroke({}, "escape", 0)
+end)
 
 require("reload")
 require("window_manager")
 require("fuzzy_window_switcher")
 -- temporarily disabled
--- require("text_expander")
+require("text_expander")
 -- require("google_meet_mic_toggle")
 
+-- require("mouse_snap_window")
 hs.alert.show("Config loaded")
