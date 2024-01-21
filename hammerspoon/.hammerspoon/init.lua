8fff596884079e8781c488b1e8e826bc804710b6
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-01-21 15:40:36 +0100

    import new hammerspoon modules

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -9,6 +9,10 @@ require("window_manager")
 require("fuzzy_window_switcher")
 require("text_expander")
 local _ = require("mouse_snap_window")
+local _ = require("scoped_hotkeys")
+local _ = require("spotify")
+helpers.loadModuleIfAvailable('personal')
+
 -- temporarily disabled
 -- require("google_meet_mic_toggle")
 
