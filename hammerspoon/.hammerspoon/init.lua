08c88a9d2ff525e68cebbe79120ed3095d2ef604
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-01-21 23:12:54 +0100

    add a basic version of chooser menu

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -18,3 +18,58 @@ local _ = helpers.loadModuleIfAvailable("personal")
 -- require("google_meet_mic_toggle")
 
 hs.alert.show("Config loaded")
+
+local function removeKeyFromTableArray(tableArray, keyToRemove)
+  local newArray = {}
+  for _, item in ipairs(tableArray) do
+    local newItem = {}
+    for key, value in pairs(item) do
+      if key ~= keyToRemove then
+        newItem[key] = value
+      end
+    end
+    table.insert(newArray, newItem)
+  end
+  return newArray
+end
+
+local function findRowById(tableArray, text)
+  for _, row in ipairs(tableArray) do
+    if row.text == text then
+      return row
+    end
+  end
+  return nil
+end
+
+local originalMenuItems = {
+  {
+    ["text"] = "Toggle Display Mirroring",
+    ["action"] = function()
+      hs.execute("~/bin/mirror")
+    end,
+  },
+  {
+    ["text"] = "Toggle Mirrored Displays",
+    ["action"] = function()
+      hs.execute("~/bin/mirror -l 1 0")
+    end,
+  },
+}
+
+local menuItems = removeKeyFromTableArray(originalMenuItems, "action")
+
+-- Function to show the menu
+local function showMenu()
+  local chooser = hs.chooser.new(function(choice)
+    if not choice then
+      return
+    end
+    findRowById(originalMenuItems, choice.text).action()
+  end)
+
+  chooser:choices(menuItems)
+  chooser:show()
+end
+
+hs.hotkey.bind({ "ctrl", "shift" }, "z", showMenu)
