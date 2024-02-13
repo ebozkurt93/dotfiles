58c9eba98117bce297e5ef69d19ac2eec91fc900
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-02-13 21:42:46 +0100

    update fuzzy_window_switcher to support more functionality

--- hammerspoon/.hammerspoon/fuzzy_window_switcher.lua
+++ hammerspoon/.hammerspoon/fuzzy_window_switcher.lua
@@ -2,8 +2,17 @@
 
 local _fuzzyChoices = nil
 local _fuzzyChooser = nil
--- local _fuzzyLastWindow = nil
+local _appFilter = nil
 local windows = {}
+
+local function resetChooser(keepFilter)
+  _fuzzyChooser:cancel()
+  _fuzzyChooser = nil
+  _fuzzyChoices = nil
+  if not keepFilter then
+    _appFilter = nil
+  end
+end
 
 local function fuzzyQuery(s, m)
   local s_len = s:len()
@@ -27,6 +36,7 @@ local function fuzzyQuery(s, m)
 end
 
 local function _fuzzyFilterChoices(query)
+  _query = query
   if query:len() == 0 then
     _fuzzyChooser:choices(_fuzzyChoices)
     return
@@ -72,42 +82,65 @@ local function handleEscape()
   end
 end
 
+local function handleAppClose()
+  local item = _fuzzyChooser:selectedRowContents()
+
+  if item == nil then
+    return
+  end
+  local app = item["app"]
+  app = hs.application.get(app)
+  app:kill()
+  resetChooser()
+end
+
 -- Register a global hotkey for Escape, but it will only have effect when _fuzzyChooser is visible
 local escapeHotkey = hs.hotkey.new({}, "escape", handleEscape)
+local closeAppHotkey = hs.hotkey.new({ "cmd" }, "q", handleAppClose)
+-- this is defined below due to avoid circular dependencies
+local filterWindowHotkey = nil
 
 local function windowFuzzySearch()
-  windows = hs.window.filter
-      .new(true)
-      :setOverrideFilter({
-        hasTitlebar = true,
-        -- allowRoles = "AXStandardWindow"
-      })
-      :getWindows(hs.window.sortByFocusedLast)
+  local filter = hs.window.filter.new(true):setOverrideFilter({
+    hasTitlebar = true,
+    -- allowRoles = "AXStandardWindow"
+  })
+
+  windows = filter:getWindows(hs.window.sortByFocusedLast)
   _fuzzyChoices = {}
   for i, w in pairs(windows) do
     local title = w:title()
     local app = w:application():name()
-    local bundleID = w:application():bundleID()
-    local icon = hs.image.imageFromAppBundle(bundleID)
-    local item = {
-      ["text"] = title,
-      ["subText"] = app,
-      ["image"] = icon,
-      ["windowID"] = w:id(),
-      ["window"] = w,
-      ["app"] = app,
-      index = i,
-    }
-    table.insert(_fuzzyChoices, item)
+    -- attempted to create a conditional filter earlier, but doesn't
+    -- work as expected, therefore this manual filtering here
+    -- also lua has no continue statement, therefore the nesting...
+    if _appFilter == nil or app == _appFilter then
+      local bundleID = w:application():bundleID()
+      local icon = hs.image.imageFromAppBundle(bundleID)
+      local item = {
+        ["text"] = title,
+        ["subText"] = app,
+        ["image"] = icon,
+        ["windowID"] = w:id(),
+        ["window"] = w,
+        ["app"] = app,
+        index = i,
+      }
+      table.insert(_fuzzyChoices, item)
+    end
   end
   _fuzzyChooser = hs.chooser.new(_fuzzyPickWindow):choices(_fuzzyChoices):searchSubText(true)
   _fuzzyChooser:queryChangedCallback(_fuzzyFilterChoices) -- Enable true fuzzy find
 
   _fuzzyChooser:showCallback(function()
     escapeHotkey:enable()
+    closeAppHotkey:enable()
+    filterWindowHotkey:enable()
   end)
   _fuzzyChooser:hideCallback(function()
     escapeHotkey:disable()
+    closeAppHotkey:disable()
+    filterWindowHotkey:disable()
   end)
 
   _fuzzyChooser:show()
@@ -115,12 +148,31 @@ end
 
 hs.hotkey.bind({ "alt" }, "space", function()
   if _fuzzyChooser then
-    _fuzzyChooser:cancel()
-    _fuzzyChooser = nil
-    _fuzzyChoices = nil
+    resetChooser()
   else
     windowFuzzySearch()
   end
 end)
+
+-- toggle window filter for selected window's app
+local function handleAppFilter()
+  if _appFilter then
+    _appFilter = nil
+  else
+    local item = _fuzzyChooser:selectedRowContents()
+
+    if item == nil then
+      return
+    end
+
+    local app = item["app"]
+    _appFilter = app
+  end
+
+  resetChooser(true)
+  windowFuzzySearch()
+end
+
+filterWindowHotkey = hs.hotkey.new({ "cmd" }, "f", handleAppFilter)
 
 return _fuzzyChooser
