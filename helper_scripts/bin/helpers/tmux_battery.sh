24dea9ef2926a70117ffe0e28344b6ee75b817fc
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-03-19 18:44:32 +0100

    fix power source detection

--- helper_scripts/bin/helpers/tmux_battery.sh
+++ helper_scripts/bin/helpers/tmux_battery.sh
@@ -1,8 +1,9 @@
 charging=$(pmset -g batt | awk -F ' ' 'gsub(";", "", $0) {print "BAT:", $3, $4}')
 charging=$(echo $charging | sed -e 's/discharging//g' -e 's/charging//g' -e 's/charged/-/g' -e 's/finishing/-/g')
+# not all these operations below are probably needed anymore, but keeping them anyway for now
 pwr_source=$(pmset -g batt | cut -c 17- | sed "s/'//g" | xargs)
 
-if [[ "$pwr_source" == 'AC Power' ]]; then
+if [[ "$pwr_source" =~ ^"AC Power" ]]; then
 	pwr_source='󱐥'
 else
 	pwr_source=''
