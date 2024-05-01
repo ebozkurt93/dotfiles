52d46d0e9ba4de124b9a696f7dd85458c809875f
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-05-01 18:55:37 +0200

    do not error if notes directory is missing

--- nvim/.config/nvim/lua/ebozkurt/obsidian.lua
+++ nvim/.config/nvim/lua/ebozkurt/obsidian.lua
@@ -1,5 +1,17 @@
+local notes_dir = os.getenv("HOME") .. "/Documents/EB-Notes-Main"
+
+local function directory_exists(path)
+  local stat = vim.loop.fs_stat(path)
+  return (stat and stat.type == "directory") or false
+end
+
+if not directory_exists(notes_dir) then
+	vim.notify("Obsidian notes directory is missing", vim.log.levels.WARN)
+	return
+end
+
 require("obsidian").setup({
-	dir = "~/Documents/EB-Notes-Main",
+	dir = notes_dir,
 	completion = {
 		nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
 	},
