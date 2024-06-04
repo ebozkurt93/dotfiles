df5bc91cd2e512e6b03d758e11132ceba6316c42
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-06-04 22:15:09 +0200

    fix keybindings for trouble.nvim

--- nvim/.config/nvim/lua/ebozkurt/keymaps.lua
+++ nvim/.config/nvim/lua/ebozkurt/keymaps.lua
@@ -362,22 +362,19 @@ vim.api.nvim_create_autocmd('User', {
 vim.api.nvim_create_autocmd('User', {
 	pattern = 'trouble',
 	callback = function()
-		vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+		vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",
 			{ silent = true, noremap = true }
 		)
-		vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+		vim.keymap.set("n", "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
 			{ silent = true, noremap = true }
 		)
-		vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+		vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>",
 			{ silent = true, noremap = true }
 		)
-		vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+		vim.keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>",
 			{ silent = true, noremap = true }
 		)
-		vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-			{ silent = true, noremap = true }
-		)
-		vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+		vim.keymap.set("n", "<leader>gr", "<cmd>Trouble lsp toggle<cr>",
 			{ silent = true, noremap = true }
 		)
 	end
