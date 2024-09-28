4d724c5b419f379c8d8dc1ba876ba0ef1259db22
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-09-28 22:06:22 +0200

    update build-neovim to use nix-shell

--- setup/build-neovim.sh
+++ setup/build-neovim.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p ninja cmake gettext curl libiconv-darwin
 
 if [[ $(uname -s) != "Darwin" ]]; then
     echo "This script is only intended to run on macOS."
     exit 1
 fi
 
-binaries=("ninja" "cmake" "gettext" "curl")
 nvim_repo_dir="$HOME/personal-repositories/neovim"
 selected_tag="${1:-nightly}"
 
@@ -14,17 +14,6 @@ function remove_neovim {
   rm ~/bin/nvim
   rm -rf ~/bin/helpers/nvim-macos
 }
-
-
-echo "Checking binaries needed for building neovim"
-for binary in "${binaries[@]}"; do
-    if command -v "$binary" &> /dev/null; then
-        echo "Dependency $binary exists"
-    else
-        echo "Dependency $binary does not exist, installing via brew"
-        brew install $binary
-    fi
-done
 
 if ! [ -d "$nvim_repo_dir" ]; then
   git clone https://github.com/neovim/neovim.git $nvim_repo_dir
@@ -40,7 +29,7 @@ git checkout $selected_tag
 remove_neovim
 
 echo 'Building neovim'
-rm -r build
+rm -rf build 
 make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=$HOME/bin/helpers/nvim-macos/
 make install
 echo 'Built neovim'
@@ -48,5 +37,3 @@ git checkout master
 
 echo 'Symlinking neovim'
 ln -s ~/bin/helpers/nvim-macos/bin/nvim ~/bin/nvim
-# todo: check if this is still needed/wanted
-rm -rf ~/bin/helpers/nvim-macos/lib/nvim/parser
