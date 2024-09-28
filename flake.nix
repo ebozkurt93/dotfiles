edf9feff72432c9e35566229e0c34f5f86d5c5c9
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-09-28 14:13:51 +0200

    initial version of flake with home manager

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+  }: {
+    homeConfigurations = {
+      erdembozkurt = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+
+        modules = [
+          (
+            {pkgs, ...}: {
+              home = {
+                packages = with pkgs; [
+                  hello
+
+                  nil # nix language server
+                  # nixfmt-rfc-style
+                  alejandra # nix formatter
+                ];
+                stateVersion = "24.05";
+                username = "erdembozkurt";
+                homeDirectory = "/Users/erdembozkurt";
+              };
+            }
+          )
+        ];
+      };
+    };
+  };
+}
