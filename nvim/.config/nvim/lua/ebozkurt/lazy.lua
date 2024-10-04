a9881743a0558e45078ab33ebfe9a2b80d5a6307
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-10-04 20:45:57 +0200

    update db_ui position

--- nvim/.config/nvim/lua/ebozkurt/lazy.lua
+++ nvim/.config/nvim/lua/ebozkurt/lazy.lua
@@ -190,7 +190,7 @@ require("lazy").setup({
     },
     init = function()
       vim.g.db_ui_use_nerd_fonts = 1
-      vim.g.db_ui_win_position = "right"
+      vim.g.db_ui_win_position = "left"
     end,
   },
   {
