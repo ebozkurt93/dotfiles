b9189fba3423f744f24e75e14e3e541605be0d9f
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-10-05 16:59:04 +0200

    add darwinbase to outputs

--- flake.nix
+++ flake.nix
@@ -6,40 +6,41 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     home-manager,
     ...
   }: {
-    homeConfigurations = let
-      darwinBase = {
-        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-        modules = [
-          (
-            {
-              pkgs,
-              lib,
-              ...
-            }: {
-              home = {
-                packages = import ./packages.nix {inherit pkgs;};
-                stateVersion = "24.05";
-                activation = lib.mkMerge [
-                  (lib.optionalAttrs (pkgs ? tmux) {
-                    installTPM = lib.mkAfter ''
-                      ${let scripts = import ./scripts.nix {inherit lib pkgs;}; in scripts.installTPM}
-                    '';
-                  })
-                ];
-              };
-            }
-          )
-        ];
-      };
-    in {
-      erdembozkurt = home-manager.lib.homeManagerConfiguration (darwinBase
+    darwinBase = {
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      modules = [
+        (
+          {
+            pkgs,
+            lib,
+            ...
+          }: {
+            home = {
+              packages = import ./packages.nix {inherit pkgs;};
+              stateVersion = "24.05";
+              activation = lib.mkMerge [
+                (lib.optionalAttrs (pkgs ? tmux) {
+                  installTPM = lib.mkAfter ''
+                    ${let scripts = import ./scripts.nix {inherit lib pkgs;}; in scripts.installTPM}
+                  '';
+                })
+              ];
+            };
+          }
+        )
+      ];
+    };
+
+    homeConfigurations = {
+      erdembozkurt = home-manager.lib.homeManagerConfiguration (self.darwinBase
         // {
           modules =
-            darwinBase.modules
+            self.darwinBase.modules
             ++ [
               ({...}: {
                 home = {
