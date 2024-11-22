428ad57edf498e7b4cf3073c0b8f055ab3d64a13
Erdem Bozkurt <ebozkurt93@gmail.com>
2024-11-22 10:03:16 +0100

    adjust wezterm overrides

--- wezterm/.config/wezterm/overrides.lua
+++ wezterm/.config/wezterm/overrides.lua
@@ -3,5 +3,10 @@ M = {}
 M.font = 'JetBrains Mono'
 -- M.window_background_opacity = 0.85
 
--- M.window_padding = { left = 0, right = 0, top = 36, bottom = 0 }
+-- local notch_enabled = true
+-- local slack_padding_enabled = true
+
+local top_padding = (notch_enabled and 36) or (slack_padding_enabled and 7) or 0
+local padding = (slack_padding_enabled and 7) or 0
+M.window_padding = { left = padding, right = padding, top = top_padding, bottom = padding }
 return M
