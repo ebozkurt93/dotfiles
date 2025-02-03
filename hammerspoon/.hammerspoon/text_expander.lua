3d01c8a0b5b8dcf29c6bbb95cd24d2ffcafeeb29
Erdem Bozkurt <ebozkurt93@gmail.com>
2025-02-03 19:20:47 +0100

    text expander updates

--- hammerspoon/.hammerspoon/text_expander.lua
+++ hammerspoon/.hammerspoon/text_expander.lua
@@ -1,149 +1,106 @@
 -- Modified version of https://github.com/Hammerspoon/hammerspoon/issues/1042
 local helpers = require("helpers")
-local _, personalKeywords = helpers.safeRequire("personal", {"expansions"}, {})
+local _, personalKeywords = helpers.safeRequire("personal", { "expansions" }, {})
 
 local keywords = {
-  ["@@lorem"] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis fermentum molestie. Vestibulum congue auctor nisi, eu ultrices lectus facilisis eu. Nulla molestie ornare massa, sed malesuada urna consequat sed. Curabitur a nibh blandit felis imperdiet interdum. Vivamus eu malesuada purus. Suspendisse in lacus non quam sagittis porttitor. In hac habitasse platea dictumst. Nullam suscipit nulla non tellus interdum faucibus. Ut eget mauris mi. Nam rhoncus quis massa sit amet placerat. Donec sollicitudin enim nec ex rutrum, in ornare arcu venenatis. Praesent consequat enim ante, et ornare eros pellentesque ut.",
+  ["@@lorem"] =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis fermentum molestie. Vestibulum congue auctor nisi, eu ultrices lectus facilisis eu. Nulla molestie ornare massa, sed malesuada urna consequat sed. Curabitur a nibh blandit felis imperdiet interdum. Vivamus eu malesuada purus. Suspendisse in lacus non quam sagittis porttitor. In hac habitasse platea dictumst. Nullam suscipit nulla non tellus interdum faucibus. Ut eget mauris mi. Nam rhoncus quis massa sit amet placerat. Donec sollicitudin enim nec ex rutrum, in ornare arcu venenatis. Praesent consequat enim ante, et ornare eros pellentesque ut.",
   ["@alph"] = function()
     return string.rep("abcdefghijklmnopqrstuvwxyz", 3)
   end,
   ["@@ip"] = function()
-    local status, body, headers = hs.http.get("https://icanhazip.com", nil)
-    return string.gsub(body, "^%s*(.-)%s*$", "%1")
-    --return body
+    local _, body = hs.http.get("https://icanhazip.com", nil)
+    return body and string.gsub(body, "^%s*(.-)%s*$", "%1") or ""
   end,
   ["@@localip"] = function()
-    return hs.network.interfaceDetails('en0')['IPv4']['Addresses'][1]
+    local details = hs.network.interfaceDetails("en0")
+    return details and details.IPv4 and details.IPv4.Addresses and details.IPv4.Addresses[1] or "Unknown"
   end,
   ["@unixtime"] = function()
     return tostring(os.time())
   end,
   ["@@time"] = function()
-    return tostring(os.date("%H:%M"))
+    return os.date("%H:%M")
   end,
   ["@@hlepoch"] = function()
-    return tostring(os.date("%y%m%d%H%M%S"))
+    return os.date("%y%m%d%H%M%S")
   end,
   ["@@hldepoch"] = function()
-    return tostring(os.date("%y-%m-%d-%H:%M:%S"))
+    return os.date("%y-%m-%d-%H:%M:%S")
   end,
 }
 
 keywords = helpers.merge(keywords, personalKeywords)
 
 local function getLastXCharacters(str, x)
-  if #str <= x then
-    return str
-  else
-    return string.sub(str, -x)
-  end
+  return #str <= x and str or string.sub(str, -x)
 end
 
 local expander = (function()
   local word = ""
   local keyMap = require("hs.keycodes").map
-  local keyWatcher
   local DEBUG = false
 
-  -- create an "event listener" function that will run whenever the event happens
-  keyWatcher = hs.eventtap
-    .new({ hs.eventtap.event.types.keyDown }, function(ev)
-      local keyCode = ev:getKeyCode()
-      local char = ev:getCharacters()
+  local keyWatcher = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(ev)
+    if ev:getFlags().cmd or ev:getFlags().ctrl or ev:getFlags().alt then
+      return false
+    end
 
-      -- if "delete" key is pressed
-      if keyCode == keyMap["delete"] then
-        if #word > 0 then
-          -- remove the last char from a string with support to utf8 characters
-          local t = {}
-          for _, chars in utf8.codes(word) do
-            table.insert(t, chars)
-          end
-          table.remove(t, #t)
-          word = utf8.char(table.unpack(t))
-          if DEBUG then
-            print("Word after deleting:", word)
-          end
-        end
-        return false -- pass the "delete" keystroke on to the application
+    local keyCode = ev:getKeyCode()
+    local char = ev:getCharacters()
+
+    if keyCode == keyMap["delete"] then
+      word = #word > 0 and string.sub(word, 1, -2) or ""
+      return false
+    end
+
+    word = getLastXCharacters(word .. char, 20)
+    local found = false
+
+    for i = #word, 1, -1 do
+      local substring = string.sub(word, i)
+      if keywords[substring] then
+        word = substring
+        found = true
+        break
+      end
+    end
+
+    if keyCode == keyMap["return"] or keyCode == keyMap["space"] or keyCode == keyMap["up"]
+        or keyCode == keyMap["down"] or keyCode == keyMap["left"] or keyCode == keyMap["right"]
+        or keyCode == keyMap["tab"] then
+      word = ""
+    end
+
+    if found then
+      for _ = 1, utf8.len(word) do
+        hs.eventtap.keyStroke({}, "delete", 0)
       end
 
-      -- append char to "word" buffer
-      word = word .. char
-      word = getLastXCharacters(word, 20)
-      local found = false
+      local replacement = keywords[word]
+      hs.eventtap.keyStrokes(type(replacement) == "function" and replacement() or replacement)
+      word = ""
+    end
 
-      local length = #word
-      for i = length, 1, -1 do
-        local substring = string.sub(word, i, length)
-        if keywords[substring] then
-          word = substring
-          found = true
-          break
-        end
-      end
+    return false
+  end):start()
 
-      -- attempt to find word from last
-      if DEBUG then
-        print("Word after appending:", word)
-      end
-
-      -- if one of these "navigational" keys is pressed
-      if
-        keyCode == keyMap["return"]
-        or keyCode == keyMap["space"]
-        or keyCode == keyMap["up"]
-        or keyCode == keyMap["down"]
-        or keyCode == keyMap["left"]
-        or keyCode == keyMap["right"]
-        or keyCode == keyMap["tab"]
-      then
-        word = "" -- clear the buffer
-      end
-
-      if DEBUG then
-        print("Word to check if hotstring:", word)
-      end
-
-      -- finally, if "word" is a hotstring
-      if found then
-        for i = 1, utf8.len(word), 1 do
-          hs.eventtap.keyStroke({}, "delete", 0)
-        end -- delete the abbreviation
-
-        if type(keywords[word]) == "function" then
-          hs.eventtap.keyStrokes(keywords[word]())
-        else
-          hs.eventtap.keyStrokes(keywords[word]) -- expand the word
-        end
-        word = "" -- clear the buffer
-      end
-
-      return false -- pass the event on to the application
-    end)
-    :start() -- start the eventtap
-
-  -- return keyWatcher to assign this functionality to the "expander" variable to prevent garbage collection
   return keyWatcher
-end)() -- this is a self-executing function because we want to start the text expander feature automatically in out init.lua
+end)()
 
 local function showExpansionsViaChooser()
   expander:stop()
   local choices = {}
   for text, _ in pairs(keywords) do
-    table.insert(choices, { ['text'] = text })
+    table.insert(choices, { text = text })
   end
 
   local chooser = hs.chooser.new(function(choice)
     if not choice then return end
-    local word = choice['text']
+    local word = choice.text
 
-    if type(keywords[word]) == "function" then
-      hs.eventtap.keyStrokes(keywords[word]())
-    else
-      hs.eventtap.keyStrokes(keywords[word])
-    end
-
+    local replacement = keywords[word]
+    hs.eventtap.keyStrokes(type(replacement) == "function" and replacement() or replacement)
   end)
 
   chooser:choices(choices)
@@ -152,8 +109,6 @@ local function showExpansionsViaChooser()
   chooser:show()
 end
 
-hs.hotkey.bind({ "cmd", "shift" }, "e", function()
-  showExpansionsViaChooser()
-end)
+hs.hotkey.bind({ "cmd", "shift" }, "e", showExpansionsViaChooser)
 
 return { expander }
