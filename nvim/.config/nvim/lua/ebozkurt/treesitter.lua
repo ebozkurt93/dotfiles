0a8821b668f3235699d2d9c5c0ba627bed4a7001
Erdem Bozkurt <ebozkurt93@gmail.com>
2025-03-27 15:04:41 +0100

    do not disable treesitter on "smaller" markdown files
    Markdown files are in general not so heavy on syntax highlighting, so
    disabling treesitter only on very large files. This might have negative
    performance indications if the markdown file contains very large code
    blocks.

--- nvim/.config/nvim/lua/ebozkurt/treesitter.lua
+++ nvim/.config/nvim/lua/ebozkurt/treesitter.lua
@@ -44,6 +44,9 @@ require("nvim-treesitter.configs").setup({
 		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
 		disable = function(lang, buf)
 			local max_filesize = 100 * 1024 -- 100 KB
+			if lang == 'markdown' then
+				max_filesize = 10000 * 1024 -- 10000 KB
+			end
 			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 			if ok and stats and stats.size > max_filesize then
 				return true
