40e7e5fb9bd250f3ce84a927ad6a5010d82428c4
Erdem Bozkurt <ebozkurt93@gmail.com>
2025-05-07 19:58:57 +0200

    remove nvim-ts-rainbow

--- nvim/.config/nvim/lua/ebozkurt/lazy.lua
+++ nvim/.config/nvim/lua/ebozkurt/lazy.lua
@@ -176,7 +176,6 @@ require("lazy").setup({
   "haydenmeade/neotest-jest",
 
   "RRethy/vim-illuminate",
-  "p00f/nvim-ts-rainbow",
   { url = "https://gitlab.com/HiPhish/rainbow-delimiters.nvim" },
   "mbbill/undotree",
 
