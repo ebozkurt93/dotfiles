1b5e6e23e57f4e1ef54320ce6c03fb77c9e478a3
Erdem Bozkurt <ebozkurt93@gmail.com>
2026-01-17 16:59:58 +0100

    add render-markdown.nvim

--- nvim/.config/nvim/lua/ebozkurt/lazy.lua
+++ nvim/.config/nvim/lua/ebozkurt/lazy.lua
@@ -205,6 +205,17 @@ require("lazy").setup({
       "stevearc/oil.nvim",
       opts = { view_options = { show_hidden = true } },
     },
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+      ---@module 'render-markdown'
+      ---@type render.md.UserConfig
+      opts = {
+        heading = { sign = false, icons = false, }
+      },
+    },
 
     -- themes
     "ellisonleao/gruvbox.nvim",
