682d134e8a4f68cbdbd02cf9feaf0fc6cbb0a0b4
Erdem Bozkurt <ebozkurt93@gmail.com>
2026-02-23 21:07:01 +0100

    do not load obsidian.lua if notes dir is missing

--- nvim/.config/nvim/lua/ebozkurt/obsidian.lua
+++ nvim/.config/nvim/lua/ebozkurt/obsidian.lua
@@ -1,5 +1,9 @@
 local notes_dir = os.getenv("HOME") .. "/Documents/EB-Notes-Main"
 
+if vim.fn.isdirectory(notes_dir) == 0 then
+	return
+end
+
 require("obsidian").setup({
 	dir = notes_dir,
 	workspaces = {
