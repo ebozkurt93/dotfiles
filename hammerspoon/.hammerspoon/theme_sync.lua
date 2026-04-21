5c4ebd2bc15177695be5d73f5d6be98c893a8a8c
Erdem Bozkurt <ebozkurt93@gmail.com>
2026-04-21 19:58:30 +0200

    remove syncClaudeTheme as claude now has auto theme

--- hammerspoon/.hammerspoon/theme_sync.lua
+++ hammerspoon/.hammerspoon/theme_sync.lua
@@ -2,11 +2,9 @@ local macos_helpers = require("macos_helpers")
 
 local watcher = hs.distributednotifications.new(function()
   macos_helpers.updateDarkModeCache()
-  macos_helpers.syncClaudeTheme()
 end, "AppleInterfaceThemeChangedNotification")
 
 watcher:start()
 macos_helpers.updateDarkModeCache()
-macos_helpers.syncClaudeTheme()
 
 return watcher
