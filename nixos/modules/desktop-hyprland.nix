8b872475353c8b9fab93a0b480b21cd11b3bbe15
Erdem Bozkurt <ebozkurt93@gmail.com>
2026-08-26 00:16:41 +0200

    fix tuigreet start to use start-hyprland

--- nixos/modules/desktop-hyprland.nix
+++ nixos/modules/desktop-hyprland.nix
@@ -28,7 +28,7 @@ in {
   services.greetd = {
     enable = true;
     settings.default_session = {
-      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
       user = "greeter";
     };
   };
